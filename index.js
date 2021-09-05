2cdadaee5ba98ff3a575130a3b3cdf298c038bb9
Angelo <angelomilienjoseph@gmail.com>
2021-09-05 09:07:14 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+
+
+  function changeElementText() {
+   ele = document.getElementById( "text" ).innerHTML = "This is really cool!";
+    return ele
+  };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    changeElementText()
+  });
+
+
+
